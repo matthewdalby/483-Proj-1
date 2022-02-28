732aac71cf3f4ba84242587ee53348b0013b4a97
KirbyNguyen <kirby.ducnguyen@outlook.com>
2022-02-27 17:47:36 -0800

    Fix KNN

--- duc_knn.py
+++ duc_knn.py
@@ -2,20 +2,22 @@ import numpy as np
 from collections import Counter
 
 
-class EuclideanKNN:
-    def __init__(self, k=5):
+class KNN:
+    def __init__(self, distance_method, k=5):
         self.k = k
+        self.distance_method = distance_method
 
     def fit(self, X, y):
         self.X_train = X
         self.y_train = y
 
+
     def predict(self, X):
         y_pred = [self._predict(x) for x in X]
         return np.array[y_pred]
 
-    def _predict(self, x, distance_method):
-        distances = [distance_method(x, x_train) for x_train in self.X_train]
+    def _predict(self, x):
+        distances = [self.distance_method(x, x_train) for x_train in self.X_train]
         k_idx = np.argsort(distances)[: self.k]
         k_neighbor_labels = [self.y_train[i] for i in k_idx]
         most_common = Counter(k_neighbor_labels).most_common(1)
