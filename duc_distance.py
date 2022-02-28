e66260fe24679b8e6e8956098c852d960fb5d26a
KirbyNguyen <kirby.ducnguyen@outlook.com>
2022-02-27 17:42:37 -0800

    Added cosine distance

--- duc_distance.py
+++ duc_distance.py
@@ -3,3 +3,10 @@ import numpy as np
 
 def euclidean_distance(point1, point2):
     return np.sqrt(np.sum((point1 - point2) ** 2))
+
+
+def cosine_distance(point1, point2):
+    sumyy = (point2 ** 2).sum(1)
+    sumxx = (point1 ** 2).sum(1, keepdims=1)
+    sumxy = point1.dot(point2.T)
+    return (sumxy / np.sqrt(sumxx)) / np.sqrt(sumyy)
